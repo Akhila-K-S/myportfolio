0495970189a492ea6f6a92dd35288ce1b23e58ba
akhilamuble <akhilamuble@gmail.com>
2023-02-01 11:45:09 +0530

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,550 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: false, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "Akhila",
+  logo_name: "akhila.ks()",
+  full_name: "Akhila K S",
+  subTitle:
+    "Software Engineer, React Native Enthusiast, Hybrid Mobile App Developer (iOS & Android).",
+  resumeLink:
+    "https://drive.google.com/file/d/11u082qdGCInQpeqR1-_c-0jZj8vqTfue/view?usp=share_link",
+};
+
+const socialMediaLinks = {
+  stackoverflow: "https://stackoverflow.com/users/19188036",
+  github: "https://github.com/Akhila-K-S",
+  linkedin: "https://www.linkedin.com/in/akhila-ks-169808204",
+  gmail: "ksakhilaofcl@gmail.com",
+  instagram: "https://instagram.com/iam.aakzz/",
+  whatsapp: "https://wa.me/917907685663?text=",
+  number: "+917907685663",
+};
+
+const contactInfo = {
+  email_address: "ksakhilaofcl@gmail.com",
+  phone_num: "+91 7907685663",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Skills and Tools",
+      fileName: "FullStackImg",
+      skills: [
+        "Development of highly interactive UI for Mobile Applications",
+        "Development & Deployment of both iOS and Android builds.",
+        "Integration of third party services such as Instana Agent, Firebase, AES Encryption, SQLite, Rich Text Editor, TTS, Text Recognition, One Signal, Google Map APIs, Social Media Feeds etc..",
+        "Bridging native modules",
+        "Propose & Implement technical solutions for complex problems",
+        "Plan source code structure & reviewing source codes",
+      ],
+      softwareSkills: [
+        {
+          skillName: "React Native",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "iOS",
+          fontAwesomeClassname: "simple-icons:ios",
+          style: {
+            color: "#A2AAAD",
+          },
+        },
+        {
+          skillName: "Android",
+          fontAwesomeClassname: "simple-icons:android",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "TypeScript",
+          fontAwesomeClassname: "simple-icons:typescript",
+          style: {
+            backgroundColor: "#007acc",
+            color: "#FFFFFF",
+          },
+        },
+        {
+          skillName: "Redux",
+          fontAwesomeClassname: "simple-icons:redux",
+          style: {
+            color: "#764abc",
+          },
+        },
+        {
+          skillName: "Redux Saga",
+          fontAwesomeClassname: "simple-icons:reduxsaga",
+          style: {
+            color: "#8AD96C",
+          },
+        },
+        {
+          skillName: "Mac",
+          fontAwesomeClassname: "simple-icons:apple",
+          style: {
+            color: "#A2AAAD",
+          },
+        },
+        {
+          skillName: "Windows",
+          fontAwesomeClassname: "simple-icons:windows",
+          style: {
+            color: "#00A4EF",
+          },
+        },
+        {
+          skillName: "Linux",
+          fontAwesomeClassname: "simple-icons:linux",
+          style: {
+            color: "#000000",
+          },
+        },
+        {
+          skillName: "VSCode",
+          fontAwesomeClassname: "simple-icons:visualstudio",
+          style: {
+            color: "#007acc",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFA611",
+          },
+        },
+        {
+          skillName: "SQLite",
+          fontAwesomeClassname: "simple-icons:sqlite",
+          style: {
+            color: "#113D52",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Cochin University of Science and Technology",
+      subtitle: "Bachelor of Technology in Computer Science and Engineering",
+      logo_path: "CusatLogo.png",
+      alt_name: "CUSAT",
+      duration: "2015 - 2019",
+      descriptions: [
+        "⚡ I have completed my B.Tech in C.S.E with a CGPA of 6.01.",
+        "⚡ Done one major and one mini project with team.",
+        "⚡ Volunteer in college tech fest",
+      ],
+      website_link: "https://www.cusat.ac.in/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "React Native Internship",
+      subtitle: "Lookings Soft",
+      logo_path: "LookingsLogo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/18c3uM8Tf-nip5MK4XoX7Oq-YntG3P6Hb/view?usp=share_link",
+      alt_name: "Lookings",
+      color_code: "#2ab0ee",
+    },
+    {
+      title: "B.Tech Certificate",
+      subtitle: "CUSAT",
+      logo_path: "CusatLogo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1J-K46DMhkzkYlrW7dE55fc1uY_fAkiwo/view?usp=sharing",
+      alt_name: "BTech",
+      color_code: "#f7b908",
+    },
+    // {
+    //   title: "Amcat Report Card",
+    //   subtitle: "Aspiring Minds",
+    //   logo_path: "AmcatLogo.png",
+    //   certificate_link:
+    //     "https://drive.google.com/file/d/1Qrrn4Tz2xAu9IJIGZmYtgbChBS0e4fVv/view?usp=sharing",
+    //   alt_name: "Amcat",
+    //   color_code: "#F7F7F7",
+    // },
+    {
+      title: "B.Tech Consolidation",
+      subtitle: "CUSAT",
+      logo_path: "CusatLogo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1JTVK1RhrI7I5cITcG_fdD8arq2YbJHIO/view?usp=sharing",
+      alt_name: "BTech Consolidation",
+      color_code: "#0C9D58",
+    },
+    // {
+    //   title: "Gate Score Card",
+    //   subtitle: "Gate 2018",
+    //   logo_path: "GateLogo.png",
+    //   certificate_link:
+    //     "https://drive.google.com/file/d/1JpCIxJJcf4udDzD1DoXckpUHi7_JCoUk/view?usp=sharing",
+    //   alt_name: "Gate",
+    //   color_code: "#fb1056",
+    // },
+    {
+      title: "HSE Certificate (12th)",
+      subtitle: "Govt of Kerala",
+      logo_path: "KeralaLogo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/18OVLeKodp228NayFjufTnX8_QRdjLxqu/view?usp=share_link",
+      alt_name: "HSE",
+      color_code: "#7A7A7A",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work and Internship",
+  description:
+    "I've 4 years of UI / Front End experience in Cross-Platform Mobile App Development using React Native.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "React Native Developer",
+          company: "Muble Solutions Pvt Ltd",
+          company_url: "https://mublesolutions.com/",
+          logo_path: "mubleLogo.png",
+          duration: "Oct 2022 - present",
+          location: "Cochin,kerala,India",
+          description: `Role: Build, develop, deploy and test major financial services, frameworks, toolsets, and features for internal financial and payments processing systems.`,
+          // color: "#fb1056",
+          color: "rgb(253 44 106 / 31%)",
+        },
+        {
+          title: "Software Engineer (React Native)",
+          company: "Edrona Learning",
+          company_url: "https://edronalearning.com/",
+          logo_path: "edLogo.jpeg",
+          duration: "Aug 2020 - Sep 2021",
+          location: "Cochin,kerala,India",
+          description: `Role: Propose & Implement technical solutions for complex problems;
+          Plan source code structure & reviewing source codes;
+          Internal evaluation of associates;
+          3rd party library integration;
+          Mentoring junior resources.`,
+          // color: "#fb1056",
+          color: "rgb(253 44 106 / 31%)",
+        },
+        {
+          title: "Mobile Developer (React Native)",
+          company: "Webandcrafts",
+          company_url: "https://webandcrafts.com/",
+          logo_path: "WacLogo.png",
+          duration: "Sep 2019 - Aug 2020",
+          location: "Thrissur,kerala,India",
+          description:
+            "Role: Team leadership; Analyse, Estimate, Design, Implement & Test Modules.",
+          // color: "#fb1056",
+          color: "rgb(253 44 106 / 31%)",
+        },
+        // {
+        //   title: "React Native Developer",
+        //   company: "Lookings Soft",
+        //   company_url: "https://lookingsoft.com/",
+        //   logo_path: "LookingsLogo.png",
+        //   duration: "June 2018 - Sep 2019",
+        //   location: "Cochin,kerala,India",
+        //   description: `Role: Design, Implement and Integrate components.`,
+        //   // color: "#fb1056",
+        //   color: "rgb(253 44 106 / 31%)",
+        // },
+      ],
+    },
+    {
+      title: "Internship",
+      experiences: [
+        {
+          title: "React Native Trainee",
+          company: "Lookings Soft",
+          company_url: "https://lookingsoft.com/",
+          logo_path: "LookingsLogo.png",
+          duration: "June 2018 - Sep 2018",
+          location: "Cochin,kerala,India",
+          description: "Training, research and understand the technology",
+          // color: "#fb1056",
+          color: "rgb(253 44 106 / 31%)",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create and develop Mobile Applications for both Android and iOS. Below are some of my projects.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "profile_photo.jpeg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others.",
+    link: "https://www.google.com",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "Medetuit",
+      url:
+        "https://play.google.com/store/apps/details?id=com.medetuit.learning",
+      description:
+        "Medetuit is a medical learning platform, offering high-quality, interactive medical learning videos adhering to the new medical curriculum.It is committed to provide the most comprehensive visual learning experience, freeing the medical students from the world of bulky books & interminable lectures.",
+      languages: [
+        {
+          name: "React Native",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Redux",
+          iconifyClass: "logos-redux",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "EDrona",
+      url: "https://play.google.com/store/apps/details?id=com.edrona",
+      description:
+        "A platform that transforms your device to a classroom. It grant quality, affordable online learning to all aspiring individuals of the society. It provide a multitude of e-learning opportunities—from individuals & private institution’s online classes & courses to job-ready certificates and degree programs.",
+      languages: [
+        {
+          name: "React Native",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+        {
+          name: "Redux",
+          iconifyClass: "logos-redux",
+        },
+        {
+          name: "Redux Saga",
+          iconifyClass: "logos-redux-saga",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "NeST Kerala Matrimony",
+      url:
+        "https://play.google.com/store/apps/details?id=com.nestmatrimony.app",
+      description:
+        "It is a large free matrimony portal and provides a happy, fulfilling, and great matchmaking experience to all Kerala youths. It places a high priority on user privacy and gives customers full control over their profiles through easy-to-use interfaces and features that help them identify, filter, and connect with potential partners.",
+      languages: [
+        {
+          name: "React Native",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Redux",
+          iconifyClass: "logos-redux",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+        // {
+        //   name: "SQLite",
+        //   iconifyClass: "logos-sqlite",
+        // },
+      ],
+    },
+    {
+      id: "3",
+      name: "Walurs - King Of Cashbacks",
+      url: "https://play.google.com/store/apps/details?id=com.walurs_mobile",
+      description:
+        "Walurs is a Canadian application for cashback & coupons site. By linking to the Canadian debit/credit card app, customers can enjoy great offers and cashback when shopping at partner sites.",
+      languages: [
+        {
+          name: "React Native",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Redux",
+          iconifyClass: "logos-redux",
+        },
+      ],
+    },
+    {
+      id: "4",
+      name: "WaiCart",
+      url: "https://waicart.com/",
+      description:
+        "The app will be primarily used for delivery purposes. A customer can order fresh produce from local farms which will be delivered to their door.",
+      languages: [
+        {
+          name: "React Native",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Firebase ML Kit",
+          iconifyClass: "logos-firebase",
+        },
+        {
+          name: "Redux",
+          iconifyClass: "logos-redux",
+        },
+        {
+          name: "React Native Maps",
+          iconifyClass: "logos-google-maps",
+        },
+      ],
+    },
+    {
+      id: "5",
+      name: "Study at Chanakya",
+      url: "https://studyatchanakya.com/",
+      description:
+        "Study at Chanakya is a student-focused, innovative, engaging student portal that offers a variety of learning content to support the student's understanding and improve the learning experience. Videos, animations, quizzes, practice tests, and answer keys will help the student to be on top of the subject. The assignments and homework will further boost their understanding of the subject and confidence as well.",
+      languages: [
+        {
+          name: "React Native",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Redux",
+          iconifyClass: "logos-redux",
+        },
+        // {
+        //   name: "One Signal",
+        //   iconifyClass: "logos-onesignal",
+        // },
+      ],
+    },
+    {
+      id: "6",
+      name: "To Do App",
+      url: "https://github.com/Akhila-K-S",
+      description:
+        "Set reminder for tasks to do on specific group to get on time local notification.",
+      languages: [
+        {
+          name: "React Native",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+  contactInfo,
+};
